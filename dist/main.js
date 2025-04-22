6a03d810ca4d2548775860982f60937ea3e21770
Sushrita Banerjee <mahesh.gaikwad@indexnine.com>
2025-04-22 15:34:16 +0530

    Added basic user class

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,33 @@
 "use strict";
-console.log("this is first line updated");
+console.log("TYPESCRIPT CODE STARTED");
+class userClass {
+    constructor() {
+        this.userArray = [];
+    }
+    getUsers() {
+        return this.userArray;
+    }
+    addUser(user) {
+        this.userArray.push(Object.assign(Object.assign({}, user), { id: this.userArray.length + 1 }));
+    }
+    verifyUser(name) {
+        const user = this.userArray.find((u) => u.name === name);
+        if (!user)
+            return false;
+        this.userArray = this.userArray.map((u) => {
+            if (u.id === user.id) {
+                u.isVerified = true;
+            }
+            return u;
+        });
+        return true;
+    }
+}
+const userData = new userClass();
+console.log("1) User Data cheking ", userData.userArray);
+userData.addUser({ name: "Mahesh Gaikwad", age: 24, isVerified: false });
+userData.addUser({ name: "Hina Kauser", age: 29, isVerified: false });
+console.log("2) User Data cheking ", userData.userArray);
+userData.verifyUser("Hina Kauser");
+console.log("3) User Data cheking ", userData.userArray);
+console.log("Is Hina Gaikwad User Exists", userData.verifyUser("Hina Gaikwad") ? "Yes" : "No");
--- main.ts
+++ main.ts
@@ -1,1 +1,53 @@
-console.log("this is first line updated");
+console.log("TYPESCRIPT CODE STARTED");
+
+interface User {
+  id?: number;
+  name: string;
+  age: number;
+  isVerified: boolean;
+}
+
+class userClass {
+  userArray: User[] = [];
+
+  constructor() {}
+
+  getUsers(): User[] {
+    return this.userArray;
+  }
+
+  addUser(user: User): void {
+    this.userArray.push({ ...user, id: this.userArray.length + 1 });
+  }
+
+  verifyUser(name: string): boolean {
+    const user: User | undefined = this.userArray.find((u) => u.name === name);
+    if (!user) return false;
+    this.userArray = this.userArray.map((u) => {
+      if (u.id === user.id) {
+        u.isVerified = true;
+      }
+      return u;
+    });
+    return true;
+  }
+}
+
+const userData = new userClass();
+
+console.log("1) User Data cheking ", userData.userArray);
+
+userData.addUser({ name: "Mahesh Gaikwad", age: 24, isVerified: false });
+
+userData.addUser({ name: "Hina Kauser", age: 29, isVerified: false });
+
+console.log("2) User Data cheking ", userData.userArray);
+
+userData.verifyUser("Hina Kauser");
+
+console.log("3) User Data cheking ", userData.userArray);
+
+console.log(
+  "Is Hina Gaikwad User Exists",
+  userData.verifyUser("Hina Gaikwad") ? "Yes" : "No"
+);
